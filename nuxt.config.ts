8159abf8e12ad113b738ccf2de3c90cdf797e38f
User <v.kushnir22@gmail.com>
2025-06-16 11:27:41 +0300

    analitics test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -54,7 +54,7 @@ export default defineNuxtConfig({
   ],
   gtag: {
     //id: 'AW-16850949987',
-    id: 'G-0RF1E5YCG5' /// my test
+    id: 'AW-17212492361' /// my test
   },
   gtm: {
     id: 'GTM-N3484GLZ'
